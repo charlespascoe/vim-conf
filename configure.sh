a35708a104cca75348aa45fe146015424dc82942
Charles Pascoe <charles@cpascoe.co.uk>
2018-09-15 17:07:22 +0100

    Tweaked configure.sh to remove python2 support

--- configure.sh
+++ configure.sh
@@ -1,12 +1,13 @@
 #sudo apt-get remove --purge vim vim-runtime vim-gnome vim-tiny vim-gui-common
 
-sudo apt-get install python-dev python3-dev libncurses5-dev
+#sudo apt-get install python3-dev libncurses5-dev
 #git clone https://github.com/vim/vim
+cd vim
 
 ./configure \
 --enable-multibyte \
---enable-pythoninterp=dynamic \
 --enable-python3interp \
+--with-python3-config-dir=/usr/lib/python3.6/config-3.6m-x86_64-linux-gnu \
 --enable-cscope \
 --with-features=huge \
 --enable-fontset \
@@ -14,7 +15,5 @@ sudo apt-get install python-dev python3-dev libncurses5-dev
 --disable-netbeans \
 --with-compiledby="Charles Pascoe" \
 --enable-fail-if-missing
-#--with-python-config-dir=/usr/lib/python2.7/config-x86_64-linux-gnu \
-#--with-python3-config-dir=/usr/lib/python3.5/config-3.5m-x86_64-linux-gnu \
 
-#make && sudo make install
+make && sudo make install
