1e02de5cecd63638a181941be1dbe9906db31695
Charles Pascoe <charles@cpascoe.co.uk>
2019-08-07 10:06:15 +0100

    configure.sh: Updated configuration flags and reordered commands

--- configure.sh
+++ configure.sh
@@ -1,13 +1,10 @@
-#sudo apt-get remove --purge vim vim-runtime vim-gnome vim-tiny vim-gui-common
-
 #sudo apt-get install python3-dev libncurses5-dev
-#git clone https://github.com/vim/vim
-cd vim
+#git clone https://github.com/vim/vim ~/repos/vim
+cd ~/repos/vim
 
 ./configure \
 --enable-multibyte \
 --enable-python3interp \
---with-python3-config-dir=/usr/lib/python3.6/config-3.6m-x86_64-linux-gnu \
 --enable-cscope \
 --with-features=huge \
 --enable-fontset \
@@ -15,5 +12,8 @@ cd vim
 --disable-netbeans \
 --with-compiledby="Charles Pascoe" \
 --enable-fail-if-missing
+#--with-python3-config-dir=/usr/lib/python3.6/config-3.6m-x86_64-linux-gnu \
 
-make && sudo make install
+#make
+#sudo apt-get remove --purge vim vim-runtime vim-gnome vim-tiny vim-gui-common
+#sudo make install
