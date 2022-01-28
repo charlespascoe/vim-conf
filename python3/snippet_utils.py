41cfd4ba666e780f4528a4857efdd2f10cf2daaf
Charles Pascoe <charles@cpascoe.co.uk>
2022-01-28 19:04:44 +0000

    snips/go_compact.snippets: add interface mock generation, various other improvements

--- python3/snippet_utils.py
+++ python3/snippet_utils.py
@@ -7,18 +7,35 @@ leading_whitespace_re = re.compile(r'^\s+')
 
 # Returns the zero-based index of the first line that matches the regexp
 def find_line(regexp):
-	for i in range(len(vim.current.window.buffer)):
-		if regexp.match(vim.current.window.buffer[i]):
-			return i
+    for i in range(len(vim.current.window.buffer)):
+        if regexp.search(vim.current.window.buffer[i]):
+            return i
 
-	return None
+    return None
 
-def preceeding_lines():
-	l = vim.current.window.cursor[0] - 2
 
-	while l >= 0:
-		yield vim.current.window.buffer[l]
-		l -= 1
+def find_lines(regexp):
+    i = 0
+
+    while i < len(vim.current.window.buffer):
+        line = vim.current.window.buffer[i]
+
+        if regexp.search(line):
+            yield (i, line)
+
+        i += 1
+
+
+# Note that line is zero-based
+def preceeding_lines(line=None):
+    if line is None:
+        line = vim.current.window.cursor[0] - 1
+
+    l = line - 1
+
+    while l >= 0:
+        yield vim.current.window.buffer[l]
+        l -= 1
 
 
 def nonempty(strings):
