574faf20137ab39bdb38deec321715776f972a43
Charles Pascoe <charles@cpascoe.co.uk>
2023-01-07 13:18:18 +0000

    python3/mdjoin.py: Improve markdown join handling of code blocks

--- python3/mdjoin.py
+++ python3/mdjoin.py
@@ -6,24 +6,42 @@ repeated_ws_re = re.compile(r"(?<=[^ \t])\s{2,}(?=[^ \t])")
 
 
 def join(start, end):
-    lines = []
-    para = []
+    return "\n".join(
+        repeated_ws_re.sub(" ", line.rstrip())
+        for line in merge(iter(vim.current.buffer[start - 1 : end]))
+    )
 
-    for line in vim.current.buffer[start - 1 : end]:
-        if line != "" and not bullet_re.match(line):
-            para.append(line)
-            continue
 
-        if len(para) > 0:
-            lines.append(" ".join(para))
-            para = []
-
-        if line != "":
-            para.append(line)
+def merge(buf_iter):
+    for line in buf_iter:
+        if line == "```":
+            yield line
+            yield from read_code(buf_iter)
         else:
-            lines.append("")
+            yield from read_block(line, buf_iter)
 
-    if len(para) > 0:
-        lines.append(" ".join(para))
 
-    return "\n".join(repeated_ws_re.sub(" ", line) for line in lines)
+def read_code(buf_iter):
+    for line in buf_iter:
+        yield line
+
+        if line == "```":
+            return
+
+
+def read_block(first, buf_iter):
+    if first == "":
+        yield ""
+        return
+
+    block = [first]
+
+    for line in buf_iter:
+        if line == "" or bullet_re.match(line):
+            yield " ".join(block)
+            yield from read_block(line, buf_iter)
+            return
+
+        block.append(line)
+
+    yield " ".join(block)
